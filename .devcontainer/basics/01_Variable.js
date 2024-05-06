82c7f7d526be0f405a055c81ab8800b65bc341b9
vardhanpatil <105341278+vardhanpatil@users.noreply.github.com>
2024-05-06 08:24:43 +0000

    datatypes

---
+++ .devcontainer/basics/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234567
+let accountEmail = "Vardhanpatil2000gmail.com"
+var accountPassword = "1234"
+accountCity = "Sangli"
+
+// accountId = 2
+// prefer not to use var 
+// beacuse of issue is block scope and function scope 
+
+
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
---
+++ .devcontainer/basics/02_Data_types.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node js, not browser
+
+console.log(3+3);
+// code readability should be high 
+console.log("Vardhan")
+
+let name = "vardhan"
+let age = 24
+let isLoggedin = false
+
+// number => 2 to power of 53
+// BigInt
+// String => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value is not assinged
+// Symbol =>unique
+
+
+// Object 
+ 
+console.log(typeof age);
+
+console.log(typeof undefined);   //undefined 
+console.log(typeof null);  //object
