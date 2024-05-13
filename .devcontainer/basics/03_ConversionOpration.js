b1e6385c563dd8990072040d336500961d854d99
vardhanpatil <105341278+vardhanpatil@users.noreply.github.com>
2024-05-13 06:52:23 +0000

    datatypesummery

--- .devcontainer/basics/03_ConversionOpration.js
+++ .devcontainer/basics/03_ConversionOpration.js
@@ -1,6 +1,6 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 // "33" => 33
 //  "33abc"=> NaN
@@ -9,11 +9,31 @@ console.log(typeof score);
 let isLoggedIn = 1
  let booleanIsLoggedIn = Boolean(isLoggedIn)
 
- console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
  // 1=> true; 0 => false
  //" => false"
 
  //vardhan = true
 
- 
+//  ##################0PERATIONS####################
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "Vardhan"
+
+let str2 = " Patil"
+
+let str3 = str1 + str2
+console.log(str3);
---
+++ .devcontainer/basics/data_typessummery.js
@@ -0,0 +1,28 @@
+//primitive data types
+
+// 7 types : String,numbers,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+
+const outsideTemp = nulll
+let userMail ;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+const bigNumber = 1234578900987654321n
+
+// Refernce TYPE = nON PRIMITIVE
+
+// Array,Objects,Functions
+
+const heros = ["shaktiman", "vardhan"];
+
+let myObj= {
+    name:"patil",
+    age:24,
+}
