454298d0440da71a64cff19ea31892579cf500e7
vardhanpatil <105341278+vardhanpatil@users.noreply.github.com>
2024-05-13 05:50:04 +0000

    coversionDatatypes

---
+++ .devcontainer/basics/03_ConversionOpration.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+console.log(typeof score);
+
+// "33" => 33
+//  "33abc"=> NaN
+// true =>1 ; false=>0
+
+let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ console.log(booleanIsLoggedIn);
+
+ // 1=> true; 0 => false
+ //" => false"
+
+ //vardhan = true
+
+ 
